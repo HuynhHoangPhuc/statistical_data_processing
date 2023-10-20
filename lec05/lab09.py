968b05e660fe95ba0d72736a9e68067d805bc339
Huynh Hoang Phuc <62494021+HuynhHoangPhuc@users.noreply.github.com>
2023-10-20 14:38:23 +0700

    Fix

--- lec05/lab09.py
+++ lec05/lab09.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+from scipy import stats
 
 
 def stats_median(vec: np.ndarray, m0: float, kind: str, level: float) -> tuple[float, str, str]:
@@ -20,6 +21,36 @@ def stats_median(vec: np.ndarray, m0: float, kind: str, level: float) -> tuple[f
     return p_value, f'm {kind} m0', 'Reject' if p_value < level else 'Not reject'
 
 
+def stats_median_with_norm(vec: np.ndarray, m0: float, kind: str, level: float) -> tuple[float, str, str]:
+    sign = vec - m0
+    sign = sign[sign != 0]
+    sign = sign > 0
+    n = np.size(sign)
+    r = sign.sum()
+    z = (r - 0.5 * n) / (0.5 * math.sqrt(n))
+    p_value = stats.norm.cdf(z, 0.5 * n, 0.25 * n)
+
+    if kind == '<':
+        p_value = p_value
+    elif kind == '>':
+        p_value = 1 - p_value
+    else:
+        p_value = 2 * (1 - p_value) if sign.sum() < n / 2 else 2 * p_value
+
+    return p_value, f'm {kind} m0', 'Reject' if p_value < level else 'Not reject'
+
+
 if __name__ == '__main__':
     data = np.loadtxt('data.txt', delimiter=' ')
-    print(stats_median(data, 2000, '=', 0.05))
+    muy0 = 2000
+
+    selection = False
+    temp = data - muy0
+    temp = temp[temp != 0]
+    if np.size(temp) >= 10:
+        selection = bool(True if input("Normal Distribution ? (y/n): ") == "y" else False)
+
+    if selection:
+        print(stats_median_with_norm(data, 2000, '=', 0.05))
+    else:
+        print(stats_median(data, 2000, '=', 0.05))
